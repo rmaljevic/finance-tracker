396e3d4648a441b33112a6438eeea671be9f233c
Robert Maljevic <158743334+RoBi233@users.noreply.github.com>
2026-09-21 21:01:12 +0200

    Add input elements for transaction management

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let transactions = [];
+
+
+const descriptionInput = document.getElementById("description");
+const amountInput = document.getElementById("amount");
+const typeInput = document.getElementById("type");
+const addButton = document.getElementById("addButton");
