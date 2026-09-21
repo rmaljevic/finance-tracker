16f658ab95eb7dc53725e2c52a5248a541c9748e
Robert Maljevic <158743334+RoBi233@users.noreply.github.com>
2026-09-21 21:03:15 +0200

    addButton

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let transactions = [];
 
-
 const descriptionInput = document.getElementById("description");
 const amountInput = document.getElementById("amount");
 const typeInput = document.getElementById("type");
 const addButton = document.getElementById("addButton");
+
+addButton.addEventListener("click", function() {
+    console.log("Button clicked!");
+});
