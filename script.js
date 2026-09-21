177b149d6bb8dee2c63a6b055b3c0dcf68bc4a4c
Robert Maljevic <158743334+rmaljevic@users.noreply.github.com>
2026-09-21 21:17:03 +0200

    Implement transaction capture on button click
    Added functionality to capture transaction details on button click.

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ const typeInput = document.getElementById("type");
 const addButton = document.getElementById("addButton");
 
 addButton.addEventListener("click", function() {
-    console.log("Button clicked!");
+
+    const description = descriptionInput.value;
+    const amount = Number(amountInput.value);
+    const type = typeInput.value;
+
+    const transaction = {
+        description: description,
+        amount: amount,
+        type: type
+    };
+
+    transactions.push(transaction);
+
+    console.log(transactions);
 });
