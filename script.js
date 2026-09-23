d857a3a4b570d5c89d73689dac5d764acdce21ca
Robert Maljevic <158743334+rmaljevic@users.noreply.github.com>
2026-09-23 21:23:57 +0200

    transaction management features

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Niz transakcija
 let transactions = [];
 
+//uzimanje elementaa sa html
 const descriptionInput = document.getElementById("description");
 const amountInput = document.getElementById("amount");
 const typeInput = document.getElementById("type");
@@ -7,6 +9,7 @@ const addButton = document.getElementById("addButton");
 const transactionList = document.getElementById("transactionList");
 const balanceDisplay = document.getElementById("balance");
 
+//upodate balance
 function updateBalance() {
 
     let balance = 0;
@@ -22,13 +25,14 @@ function updateBalance() {
 
     balanceDisplay.textContent = "€" + balance;
 }
-
+//Dodati novi transakciju
 addButton.addEventListener("click", function() {
 
     const description = descriptionInput.value;
     const amount = Number(amountInput.value);
     const type = typeInput.value;
 
+    //Pravljenje objekta transakcije
     const transaction = {
         description: description,
         amount: amount,
@@ -37,15 +41,18 @@ addButton.addEventListener("click", function() {
 
     transactions.push(transaction);
 
+    //Pravljenje premdemta ili ti stavke u listi
     const listItem = document.createElement("li");
 
     listItem.textContent = description + " - €" + amount + " (" + type + ")";
 
+    //dugme za brisanje
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
 
     listItem.appendChild(deleteButton);
-
+    
+    //Brisanje transakcije
     deleteButton.addEventListener("click", function() {
 
         const index = transactions.indexOf(transaction);
@@ -53,14 +60,15 @@ addButton.addEventListener("click", function() {
         transactions.splice(index, 1);
 
         listItem.remove();
-
+        
+    //Update posle brisanja
         updateBalance();
     });
-
+//Dodavanje transakcije u listu
     transactionList.appendChild(listItem);
-
+//Update stanja
     updateBalance();
-
+//Brisanje iz inputa
     descriptionInput.value = "";
     amountInput.value = "";
 });
